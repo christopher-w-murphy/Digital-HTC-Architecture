a08b5a25a0b104c652000ec42d6f29b46a408adf
christopher-w-murphy <murphtron5000@gmail.com>
2022-06-29 13:48:05 -0400

    ensure file names have correct extensions

--- src/application/translator_app.py
+++ src/application/translator_app.py
@@ -3,13 +3,21 @@ from src.infrastructure.configuration import get_deepl_auth_key
 from src.infrastructure.deepl import translate_document
 
 
-def main():
+def text_file_with_extension(text_file: str) -> str:
+    if not text_file.endswith('.txt'):
+        return text_file + '.txt'
+    return text_file
+
+
+def main() -> None:
     parser = construct_parser()
     args = parser.parse_args()
+    input_file = text_file_with_extension(args.input_path)
+    output_file = text_file_with_extension(args.output_path)
 
     auth_key = get_deepl_auth_key()
 
-    translate_document(args.input_path, args.output_path, auth_key)
+    translate_document(input_file, output_file, auth_key)
 
 
 if __name__ == '__main__':
