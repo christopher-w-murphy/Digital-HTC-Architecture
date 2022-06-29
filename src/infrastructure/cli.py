4f69e80a53f26210648704f84824bce7bdb129c0
christopher-w-murphy <murphtron5000@gmail.com>
2022-06-29 13:50:49 -0400

    added detail to the arg parser

--- src/infrastructure/cli.py
+++ src/infrastructure/cli.py
@@ -3,6 +3,6 @@ from argparse import ArgumentParser
 
 def construct_parser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument('input_path')
-    parser.add_argument('output_path')
+    parser.add_argument('input_path', help='The name of the OCR file.', type=str)
+    parser.add_argument('output_path', help='The name of the translated file.', type=str)
     return parser
