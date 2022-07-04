435dfb748dd6e6f50d88f290f12cb0ca7914ac42
christopher-w-murphy <murphtron5000@gmail.com>
2022-07-04 19:52:48 -0400

    remove excess line breaks from Tesseract

--- src/domain/ocr.py
+++ src/domain/ocr.py
@@ -1,17 +1,14 @@
 from PIL import Image
 from pytesseract import image_to_string
 
-from src.domain.text_processing import remove_line_breaks
+from src.domain.text_processing import remove_excess_line_breaks
 
 
-def image_to_ocr_string(img: Image, unwrap_words: bool = True) -> str:
+def image_to_ocr_string(img: Image) -> str:
     text_pages = list()
     for page in range(img.n_frames):
         img.seek(page)
         text_page = image_to_string(img, lang='fra')
         text_pages.append(text_page)
     text = '\n'.join(text_pages)
-
-    if unwrap_words:
-        return remove_line_breaks(text)
-    return text
+    return remove_excess_line_breaks(text)
