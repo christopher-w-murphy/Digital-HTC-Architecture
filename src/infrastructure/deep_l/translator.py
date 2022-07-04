a2af67d2e8c0be3d6f7dc8d92baee909f04d5194
christopher-w-murphy <murphtron5000@gmail.com>
2022-07-04 10:41:58 -0400

    separated out get translator functionality

--- src/infrastructure/deep_l/translator.py
+++
@@ -1,8 +0,0 @@
-from deepl import Translator
-
-from src.infrastructure.deep_l.configuration import get_deepl_auth_key
-
-
-def get_translator() -> Translator:
-    auth_key = get_deepl_auth_key()
-    return Translator(auth_key)
