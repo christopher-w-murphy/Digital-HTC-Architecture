b34d96d3c0d8191d50071ee9faa7d7fbe186f809
christopher-w-murphy <murphtron5000@gmail.com>
2022-07-04 10:40:11 -0400

    created streamlit frontend

--- src/infrastructure/cli.py
+++
@@ -1,7 +0,0 @@
-from argparse import ArgumentParser
-
-
-def construct_parser() -> ArgumentParser:
-    parser = ArgumentParser()
-    parser.add_argument('input_pdf_path', help='The path to the PDF to OCR and translate.', type=str)
-    return parser
---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+from src.frontend.streamlit import app
+
+
+if __name__ == '__main__':
+    app()
