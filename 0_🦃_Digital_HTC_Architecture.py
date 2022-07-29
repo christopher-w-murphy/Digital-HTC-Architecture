75acfa966c5c45eaacc2b8802a1cf9675416f7dc
christopher-w-murphy <murphtron5000@gmail.com>
2022-07-28 22:27:12 -0400

    changed name of main streamlit app

--- 0_🦃_Digital_HTC_Architecture.py
+++ 0_🦃_Digital_HTC_Architecture.py
@@ -8,7 +8,9 @@ st.write("# Welcome to Digital HTC Architecture! 🦃")
 st.markdown(
     """
     Digital HTC Architecture is a simple program for OCR and Machine Translation.
+
     **👈 Select an option from the sidebar** to see what Digital HTC Architecture can do!
+    
     ### Want to learn more?
     - Check out the README on [Github](https://github.com/christopher-w-murphy/Digital-HTC-Architecture)
 """
--- streamlit_app.py
+++
@@ -1,5 +0,0 @@
-from src.frontend.streamlit import app
-
-
-if __name__ == '__main__':
-    app()
